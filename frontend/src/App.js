e96cf9c880aa604d10bf6da10d0e7d069a75f882
alicia <aliciachen10@gmail.com>
2023-04-02 20:58:59 -0700

    change backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 
 const nhost = new NhostClient({
-  subdomain: "tbadshvjfkcftccbsjch",
+  subdomain: "hwgafkbbecxxcxgnptvr",
   region: "us-east-1"
   // subdomain: process.env.REACT_APP_NHOST_SUBDOMAIN,
   // region: process.env.REACT_APP_NHOST_REGION
